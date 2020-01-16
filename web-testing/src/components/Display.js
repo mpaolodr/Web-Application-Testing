2db1f25654b9e929b6d3af508a0ee5aab29b6b34
mpaolodr <mpaolodr_1504@yahoo.com>
2020-01-16 16:42:52 -0500

    added display.js component

---
+++ web-testing/src/components/Display.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Display = props => {
+  return (
+    <div className="display-container">
+      <div className="scoreboard">
+        <div className="home"></div>
+        <div className="away"></div>
+      </div>
+      <div className="info">
+        <div className="info-container">
+          <div className="ball">
+            <h2 className="count">{props.display.ball}</h2>
+          </div>
+          <h2 className="info-name">BALL</h2>
+        </div>
+
+        <div className="info-container">
+          <div className="strike">
+            <h2 className="count">{props.display.strike}</h2>
+          </div>
+          <h2 className="info-name">STRIKE</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Display;
