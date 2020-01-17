fbc7c73a02f970e39e672172e40ce7daf5d844e5
mpaolodr <mpaolodr_1504@yahoo.com>
2020-01-16 21:10:04 -0500

    added basic styling to make it a little presentable

--- web-testing/src/components/Display.js
+++ web-testing/src/components/Display.js
@@ -4,8 +4,19 @@ const Display = props => {
   return (
     <div className="display-container">
       <div className="scoreboard">
-        <div className="home"></div>
-        <div className="away"></div>
+        <div className="home">
+          <h2 className="team-name">Home</h2>
+          <div className="home-score">
+            <h1>{props.display.homeScore}</h1>
+          </div>
+        </div>
+
+        <div className="away">
+          <h2 className="team-name">Away</h2>
+          <div className="away-score">
+            <h1>{props.display.awayScore}</h1>
+          </div>
+        </div>
       </div>
       <div className="info">
         <div className="info-container">
