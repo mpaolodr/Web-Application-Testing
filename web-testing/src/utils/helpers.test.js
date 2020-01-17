a92814c90eb7d8e3084a89489a25602e30a71bcb
mpaolodr <mpaolodr_1504@yahoo.com>
2020-01-16 20:35:23 -0500

    added more assertions for helpers.test.js

--- web-testing/src/utils/helpers.test.js
+++ web-testing/src/utils/helpers.test.js
@@ -3,6 +3,18 @@ import { ballValue, strikeValue, hitBtn, foulBtn } from "./helpers";
 describe("Button Functionality", () => {
   test("Add Value To Ball", () => {
     expect(ballValue("", "")).toEqual({ ball: "⚾" });
+    expect(ballValue("x", "")).toEqual({ ball: "⚾" });
+    expect(ballValue("x", "⚾")).toEqual({ ball: "⚾⚾" });
+    expect(ballValue("x", "⚾⚾")).toEqual({ ball: "⚾⚾⚾" });
+    expect(ballValue("x", "⚾⚾⚾")).toEqual({ ball: "", strike: "" });
+    expect(ballValue("xx", "")).toEqual({ ball: "⚾" });
+    expect(ballValue("xx", "⚾")).toEqual({ ball: "⚾⚾" });
+    expect(ballValue("xx", "⚾⚾")).toEqual({ ball: "⚾⚾⚾" });
+    expect(ballValue("xx", "⚾⚾⚾")).toEqual({ ball: "", strike: "" });
+    expect(ballValue("xxx", "")).toEqual({ ball: "", strike: "" });
+    expect(ballValue("xxx", "⚾")).toEqual({ ball: "", strike: "" });
+    expect(ballValue("xxx", "⚾⚾")).toEqual({ ball: "", strike: "" });
+    expect(ballValue("xxx", "⚾⚾⚾")).toEqual({ ball: "", strike: "" });
   });
 
   test("Add Value To Strike", () => {
